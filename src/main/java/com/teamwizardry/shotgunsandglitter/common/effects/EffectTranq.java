f231c0a00936fe746b35fb3f22fbb0352c23f1c4
Demoniaque <saadodi44@gmail.com>
2018-03-31 21:14:53 +0300

    tranq glitter

--- src/main/java/com/teamwizardry/shotgunsandglitter/common/effects/EffectTranq.java
+++ src/main/java/com/teamwizardry/shotgunsandglitter/common/effects/EffectTranq.java
@@ -4,6 +4,7 @@ import com.teamwizardry.librarianlib.features.math.interpolate.StaticInterp;
 import com.teamwizardry.librarianlib.features.particle.ParticleBuilder;
 import com.teamwizardry.librarianlib.features.particle.ParticleSpawner;
 import com.teamwizardry.librarianlib.features.particle.functions.InterpColorHSV;
+import com.teamwizardry.librarianlib.features.particle.functions.InterpFadeInOut;
 import com.teamwizardry.shotgunsandglitter.api.Effect;
 import com.teamwizardry.shotgunsandglitter.api.util.InterpScale;
 import com.teamwizardry.shotgunsandglitter.api.util.RandUtil;
@@ -14,7 +15,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -40,7 +43,25 @@ public class EffectTranq implements Effect {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void renderImpact(@NotNull World world, @NotNull EntityBullet bullet, @NotNull RayTraceResult hit) {
+		ParticleBuilder glitter = new ParticleBuilder(10);
+		glitter.setRender(ClientEventHandler.SPARKLE);
+		glitter.disableMotionCalculation();
+		glitter.setCollision(true);
 
+		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(bullet.getPositionVector()), 100, 0, (i, build) -> {
+			build.setLifetime(RandUtil.nextInt(20, 40));
+			build.setColorFunction(new InterpColorHSV(Color.GREEN, Color.CYAN));
+			build.setAlphaFunction(new InterpFadeInOut(0f, 1f));
+			build.setScaleFunction(new InterpScale(1f, 0));
+			build.setDeceleration(new Vec3d(0.7, 0.7, 0.7));
+
+			double radius = 2 * RandUtil.nextFloat();
+			double theta = 2.0f * (float) Math.PI * RandUtil.nextFloat();
+			double x = radius * MathHelper.cos((float) theta);
+			double z = radius * MathHelper.sin((float) theta);
+
+			build.setMotion(new Vec3d(x, RandUtil.nextDouble(-2, 2), z));
+		});
 	}
 
 	@Override
@@ -53,10 +74,8 @@ public class EffectTranq implements Effect {
 		glitter.disableRandom();
 
 		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(bullet.getPositionVector()), 1, 0, (i, build) -> {
-			build.setLifetime(RandUtil.nextInt(10, 30));
-			build.setScaleFunction(new InterpScale(RandUtil.nextFloat(0.2f, 0.5f), 0));
+			build.setScaleFunction(new InterpScale(0.5f, 0));
 			build.setColorFunction(new InterpColorHSV(Color.GREEN, Color.CYAN));
-			//build.setMotion(new Vec3d(bullet.motionX, bullet.motionY, bullet.motionZ));
 		});
 	}
 }
