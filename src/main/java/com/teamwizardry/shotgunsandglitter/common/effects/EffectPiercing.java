361214289636b4c90d9462c5a68fd5924691a243
Demoniaque <saadodi44@gmail.com>
2018-03-31 13:38:47 +0300

    piercing glitter done

--- src/main/java/com/teamwizardry/shotgunsandglitter/common/effects/EffectPiercing.java
+++ src/main/java/com/teamwizardry/shotgunsandglitter/common/effects/EffectPiercing.java
@@ -1,11 +1,21 @@
 package com.teamwizardry.shotgunsandglitter.common.effects;
 
+import com.teamwizardry.librarianlib.features.math.interpolate.StaticInterp;
+import com.teamwizardry.librarianlib.features.math.interpolate.position.InterpCircle;
+import com.teamwizardry.librarianlib.features.particle.ParticleBuilder;
+import com.teamwizardry.librarianlib.features.particle.ParticleSpawner;
 import com.teamwizardry.shotgunsandglitter.api.Effect;
+import com.teamwizardry.shotgunsandglitter.api.util.InterpScale;
+import com.teamwizardry.shotgunsandglitter.api.util.RandUtil;
+import com.teamwizardry.shotgunsandglitter.client.ClientEventHandler;
 import com.teamwizardry.shotgunsandglitter.common.entity.EntityBullet;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 public class EffectPiercing implements Effect {
 
@@ -22,11 +32,40 @@ public class EffectPiercing implements Effect {
 
 	@Override
 	public void renderImpact(@NotNull World world, @NotNull EntityBullet bullet, @NotNull RayTraceResult hit) {
+		ParticleBuilder glitter = new ParticleBuilder(10);
+		glitter.setRender(ClientEventHandler.SPARKLE);
+		glitter.setCollision(true);
+		glitter.setCanBounce(true);
+		glitter.disableMotionCalculation();
+		glitter.setColor(Color.YELLOW);
 
+		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(bullet.getPositionVector()), 50, 0, (i, build) -> {
+			build.setLifetime(RandUtil.nextInt(10, 30));
+			build.setScaleFunction(new InterpScale(RandUtil.nextFloat(0.8f, 1.4f), 0));
+
+			build.setMotion(new Vec3d(
+					RandUtil.nextDouble(-0.3, 0.3),
+					RandUtil.nextDouble(-0.3, 0.6),
+					RandUtil.nextDouble(-0.3, 0.3)
+			));
+		});
 	}
 
 	@Override
 	public void renderUpdate(@NotNull World world, @NotNull EntityBullet bullet) {
+		ParticleBuilder glitter = new ParticleBuilder(10);
+		glitter.setRender(ClientEventHandler.SPARKLE);
+		glitter.setCollision(true);
+		glitter.setCanBounce(true);
+		glitter.disableMotionCalculation();
+		glitter.setColor(Color.YELLOW);
 
+		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(bullet.getPositionVector()), 15, 0, (i, build) -> {
+			build.setLifetime(RandUtil.nextInt(10, 30));
+			build.setScaleFunction(new InterpScale(RandUtil.nextFloat(0.1f, 0.5f), 0));
+
+			Vec3d position = new InterpCircle(Vec3d.ZERO, new Vec3d(bullet.motionX, bullet.motionY, bullet.motionZ).normalize(), 1, 50, 0).get(RandUtil.nextFloat());
+			build.setMotion(position.scale(1.0 / 10.0));
+		});
 	}
 }
