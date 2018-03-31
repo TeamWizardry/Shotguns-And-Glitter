115ea3cc5b1e719c98f03ec856d06d4ea54fc968
Demoniaque <saadodi44@gmail.com>
2018-03-31 21:00:16 +0300

    flash glitter

--- src/main/java/com/teamwizardry/shotgunsandglitter/common/effects/EffectFlash.java
+++ src/main/java/com/teamwizardry/shotgunsandglitter/common/effects/EffectFlash.java
@@ -1,15 +1,27 @@
 package com.teamwizardry.shotgunsandglitter.common.effects;
 
+import com.teamwizardry.librarianlib.features.math.interpolate.StaticInterp;
+import com.teamwizardry.librarianlib.features.math.interpolate.position.InterpBezier3D;
+import com.teamwizardry.librarianlib.features.particle.ParticleBuilder;
+import com.teamwizardry.librarianlib.features.particle.ParticleSpawner;
+import com.teamwizardry.librarianlib.features.particle.functions.InterpColorHSV;
+import com.teamwizardry.librarianlib.features.particle.functions.InterpFadeInOut;
 import com.teamwizardry.shotgunsandglitter.api.Effect;
+import com.teamwizardry.shotgunsandglitter.api.util.InterpScale;
+import com.teamwizardry.shotgunsandglitter.api.util.RandUtil;
+import com.teamwizardry.shotgunsandglitter.client.ClientEventHandler;
 import com.teamwizardry.shotgunsandglitter.common.entity.EntityBullet;
 import com.teamwizardry.shotgunsandglitter.common.potions.ModPotions;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
 
 public class EffectFlash implements Effect {
 
@@ -48,11 +60,56 @@ public class EffectFlash implements Effect {
 
 	@Override
 	public void renderImpact(@NotNull World world, @NotNull EntityBullet bullet, @NotNull RayTraceResult hit) {
+		ParticleBuilder glitter = new ParticleBuilder(10);
+		glitter.setRender(ClientEventHandler.SPARKLE);
+		glitter.setCollision(true);
+		glitter.setCanBounce(true);
 
+		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(bullet.getPositionVector()), 100, 0, (i, build) -> {
+			build.setLifetime(RandUtil.nextInt(50, 100));
+			build.setScaleFunction(new InterpScale(RandUtil.nextFloat(1f, 3f), 0));
+			build.setColorFunction(new InterpColorHSV(new Color(0xc1ffec), new Color(0xfffac1)));
+			build.setAcceleration(new Vec3d(0, RandUtil.nextDouble(-0.05, -0.1), 0));
+			build.setAlphaFunction(new InterpFadeInOut(0, 1f));
+
+			build.setDeceleration(new Vec3d(0.9, 0.9, 0.9));
+
+			double radius = RandUtil.nextDouble(3, 5);
+			double theta = 2.0f * (float) Math.PI * RandUtil.nextFloat();
+			double r = radius * RandUtil.nextFloat();
+			double x = r * MathHelper.cos((float) theta);
+			double z = r * MathHelper.sin((float) theta);
+			build.setMotion(new Vec3d(x, RandUtil.nextDouble(-radius, radius), z));
+		});
 	}
 
 	@Override
 	public void renderUpdate(@NotNull World world, @NotNull EntityBullet bullet) {
+		ParticleBuilder glitter = new ParticleBuilder(10);
+		glitter.setRender(ClientEventHandler.SPARKLE);
+		glitter.setCollision(true);
 
+		ParticleSpawner.spawn(glitter, world, new StaticInterp<>(bullet.getPositionVector()), 1, 0, (i, build) -> {
+			build.setLifetime(RandUtil.nextInt(5, 20));
+			build.setScaleFunction(new InterpScale(RandUtil.nextFloat(1f, 3f), 0));
+			build.setColorFunction(new InterpColorHSV(new Color(0xc1ffec), new Color(0xfffac1)));
+			build.setMotion(new Vec3d(bullet.motionX, bullet.motionY, bullet.motionZ));
+			build.setPositionFunction(new InterpBezier3D(Vec3d.ZERO,
+					new Vec3d(
+							RandUtil.nextDouble(-0.2, 0.2),
+							RandUtil.nextDouble(-0.2, 0.2),
+							RandUtil.nextDouble(-0.2, 0.2)
+					),
+					new Vec3d(
+							RandUtil.nextDouble(-1, 1),
+							RandUtil.nextDouble(-1, 1),
+							RandUtil.nextDouble(-1, 1)
+					),
+					new Vec3d(
+							RandUtil.nextDouble(-1, 1),
+							RandUtil.nextDouble(-1, 1),
+							RandUtil.nextDouble(-1, 1)
+					)));
+		});
 	}
 }
